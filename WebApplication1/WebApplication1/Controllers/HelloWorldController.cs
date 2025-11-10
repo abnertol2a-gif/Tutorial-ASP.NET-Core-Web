80479dab8e162b73795f1bea6d1ce56630b6b748
abnertol2a-gif <abnertol2a@gmail.com>
2025-11-09 18:43:40 -0600

    Tutorial de microsoft paso 3, se agrega una vista

--- WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
+++ WebApplication1/WebApplication1/Controllers/HelloWorldController.cs
@@ -7,9 +7,9 @@ public class HelloWorldController : Controller
 {
     // 
     // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
     // 
     // GET: /HelloWorld/Welcome/ 
@@ -20,8 +20,10 @@ public class HelloWorldController : Controller
 
     // GET: /HelloWorld/Welcome/ 
     // Requires using System.Text.Encodings.Web;
-    public string Welcome(string name, int ID = 1)
+    public IActionResult Welcome(string name, int numTimes = 1)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        ViewData["Message"] = "Hello " + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
