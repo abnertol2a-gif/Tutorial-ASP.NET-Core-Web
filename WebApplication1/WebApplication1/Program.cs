1df8d9206959d280419e86baf6d98ae2e1252c13
abnertol2a-gif <abnertol2a@gmail.com>
2025-11-20 21:41:47 -0600

    Paso 5 del tutorial de microsoft. Se agregan datos a la BD

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Models;
 using WebApplication1.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<WebApplication1Context>(options =>
@@ -9,6 +10,13 @@ builder.Services.AddDbContext<WebApplication1Context>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
