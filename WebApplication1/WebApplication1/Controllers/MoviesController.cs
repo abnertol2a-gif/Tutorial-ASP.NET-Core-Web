e64558b5f3bc3d63a8b59508c6a03ad6cdc695fc
abnertol2a-gif <abnertol2a@gmail.com>
2025-11-25 21:21:00 -0600

    Paso 8 del tutorial de microsoft. Se agrega un nuevo campo llamado Rating. Se modifican los respectivos controladores.

--- WebApplication1/WebApplication1/Controllers/MoviesController.cs
+++ WebApplication1/WebApplication1/Controllers/MoviesController.cs
@@ -90,7 +90,7 @@ namespace WebApplication1.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -102,6 +102,7 @@ namespace WebApplication1.Controllers
         }
 
         // GET: Movies/Edit/5
+
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -122,7 +123,7 @@ namespace WebApplication1.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (id != movie.Id)
             {
